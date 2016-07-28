66a402486071b70a8e8413a31520e110c29209ed
Miles Hollingsworth <mhmiles@me.com>
2016-07-28 09:57:50 -0700

    Initial Commit

---
+++ SwiftSSDPClient/SwiftSSDPClient.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSSDPClient.h
+//  SwiftSSDPClient
+//
+//  Created by Miles Hollingsworth on 7/28/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSSDPClient.
+FOUNDATION_EXPORT double SwiftSSDPClientVersionNumber;
+
+//! Project version string for SwiftSSDPClient.
+FOUNDATION_EXPORT const unsigned char SwiftSSDPClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSSDPClient/PublicHeader.h>
+
+
